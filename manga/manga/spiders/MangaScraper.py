31c1b307664ce56672d42441327fd33e64c7f168
Mazdeus <fadhilcr1@gmail.com>
2024-04-21 18:11:24 +0700

    Update MangaScraper

--- manga/manga/spiders/MangaScraper.py
+++ manga/manga/spiders/MangaScraper.py
@@ -5,23 +5,7 @@ class MangaSpider(scrapy.Spider):
     name = "manga-spider"
     start_urls = ["https://myanimelist.net/topmanga.php"]
 
-    def parse(self, response, **kwargs):
-        for mangas in response.css('tr.ranking-list'):
-            detail_url = mangas.css('a.hoverinfo_trigger.fs14.fw-b::attr(href)').get()
-            yield response.follow(detail_url, self.parse_detail, meta={
-                'rank': mangas.css('span.lightLink.top-anime-rank-text::text').get(),
-                'title': mangas.css('a.hoverinfo_trigger.fs14.fw-b::text').get(),
-                'image': mangas.css('img.lazyload::attr(data-src)').get(),
-                'information': mangas.css('div.information::text').getall(),
-                'score': mangas.css('div.js-top-ranking-score-col span.text::text').get(),
-            })
-
-        next_page = response.css("a.link-blue-box.next::attr(href)").get()
-        if next_page is not None:
-            next_page_url = response.urljoin(next_page)
-            yield response.follow(next_page_url, callback=self.parse)
-
-    def parse_detail(self, response):
+    def parse_detail(self, response, **kwargs):
         yield {
             'rank': response.meta['rank'],
             'title': response.meta['title'],
@@ -35,6 +19,13 @@ class MangaSpider(scrapy.Spider):
             'author': response.css('div.spaceit_pad:contains("Authors:") a::text').getall(),
         }
 
+
+        next_page = response.css("a.link-blue-box.next::attr(href)").get()
+        if next_page is not None:
+            next_page_url = response.urljoin(next_page)
+            yield response.follow(next_page_url, callback=self.parse)
+
+    
 
 
 
