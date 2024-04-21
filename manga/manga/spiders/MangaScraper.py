2f3835f5c08c91ce232d05388f1aceff6ef79a21
rioBMO <harioobmmo@gmail.com>
2024-04-21 22:53:25 +0700

    Update Scraper

--- manga/manga/spiders/MangaScraper.py
+++ manga/manga/spiders/MangaScraper.py
@@ -1,27 +1,28 @@
 import scrapy
+
+
 class MangaSpider(scrapy.Spider):
     name = "manga-spider"
     start_urls = ["https://myanimelist.net/topmanga.php"]
-    manga_count = 0
 
-    def parse(self, response):
+    def parse(self, response, **kwargs):
         for mangas in response.css('tr.ranking-list'):
-            rank = 'span.lightLink.top-anime-rank-text::text'
-            self.manga_count += 1
-            if self.manga_count <= 1000:
-                yield{
-                    'rank': mangas.css(rank).get(),
-                    'title':mangas.css('a.hoverinfo_trigger.fs14.fw-b::text').get(),
-                    'image': mangas.css('img.lazyload::attr(data-src)').get(),
-                    'information': mangas.css('div.information::text').getall(),
-                    'score': mangas.css('div.js-top-ranking-score-col span.text::text').get(),
-                }
+            detail_url = mangas.css('a.hoverinfo_trigger.fs14.fw-b::attr(href)').get()
+            yield response.follow(detail_url, self.parse_detail, meta={
+                'rank': mangas.css('span.lightLink.top-anime-rank-text::text').get(),
+                'title': mangas.css('a.hoverinfo_trigger.fs14.fw-b::text').get(),
+                'image': mangas.css('img.lazyload::attr(data-src)').get(),
+                'information': mangas.css('div.information::text').getall(),
+                'score': mangas.css('div.js-top-ranking-score-col span.text::text').get(),
+            })
+
         next_page = response.css("a.link-blue-box.next::attr(href)").get()
-        if next_page is not None and self.manga_count <= 1000:
-            next_page_url = "https://myanimelist.net/topmanga.php"+next_page
-            yield response.follow(next_page_url, callback = self.parse)
+        if next_page is not None:
+            next_page_url = response.urljoin(next_page)
+            yield response.follow(next_page_url, callback=self.parse)
 
     def parse_detail(self, response):
+        
         yield {
             'rank': response.meta['rank'],
             'title': response.meta['title'],
